61adaecf8facfe47c048823a586553d9f1b8e6a5
Tim Oltjenbruns <timothyolt@gmail.com>
2023-11-23 07:58:48 -0600

    raise when mid value pair

--- src/main/kotlin/poker/player/kotlin/Player.kt
+++ src/main/kotlin/poker/player/kotlin/Player.kt
@@ -2,6 +2,7 @@ package poker.player.kotlin
 
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.ceil
 
 class Player {
     fun betRequest(game_state: JSONObject): Int {
@@ -13,16 +14,23 @@ class Player {
         val second = holeCards[1] as JSONObject
 
         println(holeCards)
-        return (if (isHighValuePair(first, second)) {
-            betAllIn(us)
-        } else {
-            fold(us)
-        })
+        return when {
+            isHighValuePair(first, second) -> betAllIn(us)
+            isMidValuePair(first, second) -> raise(game_state)
+            else -> fold(us)
+        }
     }
+    private fun isMidValuePair(first: JSONObject, second: JSONObject) =
+        isPair(first, second) && (first["rank"] == "10" || first["rank"] == "J" || first["rank"] == "Q")
 
     private fun fold(player: JSONObject) = 0
 
     private fun betAllIn(player: JSONObject): Int = player["stack"] as Int
+
+    private fun raise(game_state: JSONObject): Int {
+        val pot = game_state["pot"] as Int
+        return ceil(pot * 2.0 / 3.0).toInt()
+    }
 
     private fun isHighValuePair(first: JSONObject, second: JSONObject) =
         isPair(first, second) && (first["rank"] == "A" || first["rank"] == "K")
