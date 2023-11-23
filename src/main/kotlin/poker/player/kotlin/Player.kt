fddf67cfbcfcd5a9a77223ef0367a478c830f6eb
J. B. Rainsberger <me@jbrains.ca>
2023-11-23 09:16:49 -0400

    Log the game state.

--- src/main/kotlin/poker/player/kotlin/Player.kt
+++ src/main/kotlin/poker/player/kotlin/Player.kt
@@ -4,6 +4,7 @@ import org.json.JSONObject
 
 class Player {
     fun betRequest(game_state: JSONObject): Int {
+        println(game_state)
         return game_state["minimum_raise"] as Int
     }
 
