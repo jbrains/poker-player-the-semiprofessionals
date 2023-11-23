3fc70c3801004dec44e9a93bd817799129cd83be
J. B. Rainsberger <me@jbrains.ca>
2023-11-23 09:18:59 -0400

    Oops.

--- src/main/kotlin/poker/player/kotlin/Player.kt
+++ src/main/kotlin/poker/player/kotlin/Player.kt
@@ -5,7 +5,7 @@ import org.json.JSONObject
 class Player {
     fun betRequest(game_state: JSONObject): Int {
         println(game_state)
-        return game_state["minimum_raise"] as Int
+        return 0
     }
 
     fun showdown() {
